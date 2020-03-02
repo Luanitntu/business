4eb9d9bbc25e0f046f9e7f4720418d7bdbf80e2e
Luanitntu <nguyenthanhluanitntu@gmail.com>
2020-03-02 17:08:41 +0700

    setup template laravel and create page sign in admin

---
+++ resources/views/signin/signin.blade.php
@@ -0,0 +1,50 @@
+@extends('layout.master')
+<div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+    <div class="wrapper wrapper--w790">
+        <div class="card card-5">
+            <div class="card-heading">
+                <h2 class="title">Event Registration Form</h2>
+            </div>
+            <div class="card-body">
+                 <form method="POST" action="{{-- {{route('login')}} --}}"><br>
+                    {{ csrf_field() }}
+                    <div class="form-row">
+                        <div class="name">Username</div>
+                        <div class="value">
+                            <div class="input-group">
+                                <input class="input--style-5" type="text" name="username">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="name">Password</div>
+                        <div class="value">
+                            <div class="input-group">
+                                <input class="input--style-5" type="password" name="password">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="name">Email</div>
+                        <div class="value">
+                            <div class="input-group">
+                                <input class="input--style-5" type="email" name="email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="name">Phone</div>
+                        <div class="value">
+                            <div class="input-group">
+                                <input class="input--style-5" type="tel" name="phone">
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn btn--radius-2 btn--red" type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
